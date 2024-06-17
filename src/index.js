3cd4a61be71c327f0c5674a4fb163deda7e148df
monddavila <59162458+monde1023@users.noreply.github.com>
2024-06-17 23:29:19 +0800

    firebase google oath implementation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, signInWithEmailAndPassword } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,4 +21,67 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-console.log(app)
+console.log(app);
+
+const googleSignInBtn = document.querySelector('.google-sign-in');
+const provider = new GoogleAuthProvider();
+const auth = getAuth(app);
+
+//Google Sign-In authentication
+googleSignInBtn.addEventListener('click', () => {
+  //console.log('Button clicked!');
+  signInWithPopup(auth, provider).then((result) => {
+    const user = result.user;
+    alert(`Hello ${user.displayName}! You signed in with Google Oauth.`);
+
+
+  }).catch((error) => {
+    const errorMessage = error.message;
+    alert(`Error: ${errorMessage}`);
+  });
+
+});
+
+const epSignInBtn = document.querySelector('.ep-sign-in');
+epSignInBtn.addEventListener('click', () => {
+  //const email = document.querySelector('.email').value;
+  //const password = document.querySelector('.password').value;
+
+  const email = "test@email.com";
+  const password = "12345678";
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      alert(`Hello ${user.email}! You signed in with email and password.`);
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert(`Error: ${errorMessage}`);
+    });
+});
+
+//Sign-Out
+const signOutBtn = document.querySelector('.sign-out');
+signOutBtn.addEventListener('click', () => {
+  signOut(auth)
+  .then(() => {
+    alert('Signed out successfully!');
+  })
+  .catch((error) => {
+    console.error('Sign out failed:', error.message);
+  });
+});
+
+
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    alert("User is signed in"); // Update UI accordingly
+  } else {
+    alert("User is signed out"); // Update UI accordingly
+  }
+});
